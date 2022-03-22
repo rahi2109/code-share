9a2d9d4545829699c6395f043f774f811c2ce3cd
rahi2109 <rahi.anshuman21@gmail.com>
2022-03-23 03:01:48 +0530

    added build index file

---
+++ build/precache-manifest.3236a4f0c2b93ba3e9ebc9344c31a8d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c89da0f9376cb6a719fb8e19fb5104d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dcd58a48d7f9d91c7b4",
+    "url": "/static/css/2.8e69e9c3.chunk.css"
+  },
+  {
+    "revision": "4c0c7f2fecc83a5152b5",
+    "url": "/static/css/main.75ca0050.chunk.css"
+  },
+  {
+    "revision": "6dcd58a48d7f9d91c7b4",
+    "url": "/static/js/2.099b4b13.chunk.js"
+  },
+  {
+    "revision": "74e0abd0c4bbbdf928adcd0ea1446132",
+    "url": "/static/js/2.099b4b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c0c7f2fecc83a5152b5",
+    "url": "/static/js/main.2ef55843.chunk.js"
+  },
+  {
+    "revision": "7ed36772726da4662b3f",
+    "url": "/static/js/runtime-main.86f2d09b.js"
+  }
+]);
